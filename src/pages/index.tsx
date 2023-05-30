4c09d2abd2c1b9a6d93de6bd013c27a1e9a4cbde
1E10 Laptop <sample@bths.edu>
2023-05-30 11:00:08 -0400

    w

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,19 @@ import {
   Stack,
   useDisclosure,
   Image,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
 } from "@chakra-ui/react";
 import Layout from "@/components/layout";
 import TradingViewWidget from "@/components/TradingViewWidget";
+import { truncate } from "fs";
 
 const Home: NextPage = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -65,49 +75,93 @@ const Home: NextPage = () => {
         </Heading>
       </Stack>
       <br></br>
-      <Stack flexDir="row">
+      <Box display="flex" flexDirection={{ base: "column", md: "row" }}>
         <Image
           src="\Apple-sues-god_thumb .jpg"
           alt="Logo"
-          w={600}
-          h={400}
+          width={1000}
+          height={600}
           p={10}
           px={20}
         />
+        <Heading>
+          <Text fontSize={25} p={10} style={{ flex: 1, flexWrap: "wrap" }}>
+            Apple Drops the Ultimate Lawsuit: Sues God for Unauthorized Creation
+            of Apple Devices in Heaven!
+            <Text
+              fontSize={18}
+              py={10}
+              fontWeight={"normal"}
+              style={{ flex: 1, flexWrap: "wrap" }}
+            >
+              In an astonishing turn of events, Apple has filed a groundbreaking
+              lawsuit, accusing God of creating apple devices in heaven without
+              proper authorization. The legal battle unfold as the tech giant
+              seeks to protect its trademark and intellectual property rights
+              even in the divine realm.
+            </Text>
+          </Text>
+        </Heading>
+      </Box>
 
-        <Text fontSize={20} p={10} style={{ flex: 1, flexWrap: "wrap" }}>
-          Apple Drops the Ultimate Lawsuit: Sues God for Unauthorized Creation
-          of Apple Devices in Heaven!
-        </Text>
-      </Stack>
-      <Stack flexDir="row">
+      <Box display="flex" flexDirection={{ base: "column", md: "row" }}>
         <Image
           src="\1607805320.webp"
           alt="Logo"
-          w={600}
-          h={400}
+          width={1100}
+          height={500}
           p={10}
           px={20}
           style={{ flex: 1, flexWrap: "wrap" }}
         />
-        <Text fontSize={20} p={10} style={{ flex: 1, flexWrap: "wrap" }}>
-          SpongeBob SquarePants Unleashes Krabby Patty Stock Craze: Wall Street
-          Sizzles in Profits
-        </Text>
-      </Stack>
-      <Stack flexDir="row">
+        <Heading>
+          <Text fontSize={20} p={10} style={{ flex: 1, flexWrap: "wrap" }}>
+            SpongeBob SquarePants Unleashes Krabby Patty Stock Craze: Wall
+            Street Sizzles in Profits
+          </Text>
+          <Text
+            fontSize={18}
+            py={10}
+            fontWeight={"normal"}
+            style={{ flex: 1, flexWrap: "wrap" }}
+          >
+            The Krabby Patty craze sends schockwaves through Wall Street as
+            investors scramble to capitalize on the skyrocketing demand for the
+            beloved fictional burger. Stocks of seafood and burger-related
+            industries surge, while financial analysts attempt to gauge the
+            long-term impact of this unexpected phenomenon on the market.
+          </Text>
+        </Heading>
+      </Box>
+      <Box display="flex" flexDirection={{ base: "column", md: "row" }}>
         <Image
           src="\b24f40c8-e212-4765-a5b1-00a0c835e181-1667420600593-pfarm-with-png-watermarked.webp"
           alt="Logo"
-          w={600}
-          h={400}
+          width={1100}
+          height={500}
           p={10}
           px={20}
+          style={{ flex: 1, flexWrap: "wrap" }}
         />
-        <Text fontSize={20} p={10} style={{ flex: 1, flexWrap: "wrap" }}>
-          Friendly Invasion of Mars Leads to Worldwide Space Research Stock Rise
-        </Text>
-      </Stack>
+        <Heading>
+          <Text fontSize={20} p={10} style={{ flex: 1, flexWrap: "wrap" }}>
+            Friendly Invasion of Mars Leads to Worldwide Space Research Stock
+            Rise
+          </Text>
+          <Text
+            fontSize={18}
+            py={10}
+            fontWeight={"normal"}
+            style={{ flex: 1, flexWrap: "wrap" }}
+          >
+            In an astonishing turn of events, Apple has filed a groundbreaking
+            lawsuit, accusing God of creating apple devices in heaven without
+            proper authorization. The legal battle unfold as the tech giant
+            seeks to protect its trademark and intellectual property rights even
+            in the divine realm.
+          </Text>
+        </Heading>
+      </Box>
     </Layout>
   );
 };
--- src/pages/watchlist/index.tsx
+++ src/pages/watchlist/index.tsx
@@ -1,57 +1,124 @@
-import React, { useState } from "react";
+import type { NextPage } from "next";
+import Layout from "@/components/layout";
+import { useEffect, useState } from "react";
+import {
+  Text,
+  Flex,
+  Button,
+  Checkbox,
+  CheckboxGroup,
+  Input,
+  Heading,
+  Stack,
+  Divider,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
+import Link from "next/link";
 
-const StockWatchlist = () => {
-  const [stocks, setStocks] = useState<string[]>([]);
+const WatchList: NextPage = () => {
+  const [watchlist, setWatchlist] = useState<string[]>(new Array());
 
-  // Function to add a stock to the watchlist
-  const addStock = (symbol: string): void => {
-    setStocks((prevStocks: string[]) => [...prevStocks, symbol]);
+  useEffect(() => {
+    const g = localStorage.getItem("groceries");
+    if (g) {
+      const items = JSON.parse(g);
+      if (items) {
+        setWatchlist(items);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("groceries", JSON.stringify(watchlist));
+  }, [watchlist]);
+
+  const handleAddItem = (): void => {
+    const newItem = (
+      document.getElementById("newItemInput") as HTMLInputElement
+    ).value;
+    if (newItem) {
+      setWatchlist((prevItems: any) => [...prevItems, newItem]);
+      (document.getElementById("newItemInput") as HTMLInputElement).value = "";
+    }
   };
 
-  // Function to remove a stock from the watchlist
-  const removeStock = (symbol: string): void => {
-    setStocks((prevStocks: string[]) =>
-      prevStocks.filter((stock: string) => stock !== symbol)
-    );
+  const handleAddRecipe = (arr: any): void => {
+    setWatchlist((prevItems: any) => [...prevItems, ...arr]);
   };
 
   return (
-    <div>
-      <h1>Stock Watchlist</h1>
-      <ul>
-        {stocks.length > 0 ? (
-          stocks.map((stock: string, index: number) => (
-            <li key={index}>
-              {stock} <button onClick={() => removeStock(stock)}>Remove</button>
-            </li>
-          ))
-        ) : (
-          <li>No stocks in watchlist. Add something!</li>
-        )}
-      </ul>
-      <div>
-        <input
-          type="text"
-          id="newStockInput"
-          placeholder="Enter stock symbol"
-        />
-        <button
-          onClick={() => {
-            const inputElement = document.getElementById(
-              "newStockInput"
-            ) as HTMLInputElement;
-            const symbol: string = inputElement.value;
-            if (symbol) {
-              addStock(symbol);
-              inputElement.value = "";
-            }
-          }}
-        >
-          Add Stock
-        </button>
-      </div>
-    </div>
+    <Layout title="WatchList">
+      <Flex
+        flexDir="column"
+        align="center"
+        justify="center"
+        px={{ base: 6, sm: 10 }}
+      >
+        <Text mx={10} my={10} fontSize="50">
+          WatchList
+        </Text>
+        <Flex flexDir="column" minW={{ base: "xs", md: "lg" }} gap={5}>
+          <Input id="newItemInput" placeholder="Type item here" />
+          <Flex flexDir="row" align="center" justify="center" gap={10}>
+            <Button onClick={handleAddItem}>Add Item</Button>
+          </Flex>
+          <CheckboxGroup>
+            {watchlist.length > 0 ? (
+              watchlist.map((watchlist, index) => (
+                <Checkbox key={index}>{watchlist}</Checkbox>
+              ))
+            ) : (
+              <Text textAlign="center">Add a stock to get started!</Text>
+            )}
+          </CheckboxGroup>
+          <TableContainer>
+            <Table variant="simple">
+              <Thead>
+                <Tr>
+                  <Th>Ticker</Th>
+                  <Th>Price</Th>
+                  <Th isNumeric>Price Change</Th>
+                  <Th isNumeric>% Change</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>inches</Td>
+                  <Td>millimetres (mm)</Td>
+                  <Td isNumeric>25.4</Td>
+                </Tr>
+                <Tr>
+                  <Td>feet</Td>
+                  <Td>centimetres (cm)</Td>
+                  <Td isNumeric>30.48</Td>
+                </Tr>
+                <Tr>
+                  <Td>yards</Td>
+                  <Td>metres (m)</Td>
+                  <Td isNumeric>0.91444</Td>
+                </Tr>
+              </Tbody>
+              <Tfoot>
+                <Tr>
+                  <Th>To convert</Th>
+                  <Th>into</Th>
+                  <Th isNumeric>multiply by</Th>
+                </Tr>
+              </Tfoot>
+            </Table>
+          </TableContainer>
+        </Flex>
+      </Flex>
+    </Layout>
   );
 };
 
-export default StockWatchlist;
+export default WatchList;
