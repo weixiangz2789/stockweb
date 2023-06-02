6f4b714bb94996f7578779b70a8b684d809a3ae7
1E10 Laptop <sample@bths.edu>
2023-06-02 10:57:32 -0400

    w

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ const Document = () => {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body style={{ backgroundColor: "black" }}>
         <Main />
         <NextScript />
       </body>
